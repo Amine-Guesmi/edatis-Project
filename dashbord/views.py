4d5e349b8d51a85374000bc5215e97fc29b7755b
Amine-Guesmi <aminegass2016@gmail.com>
2020-04-30 02:02:21 +0100

    test is file readable or not

--- dashbord/views.py
+++ dashbord/views.py
@@ -3,7 +3,7 @@ import os, subprocess
 
 # Create your views here.
 def dashbord(request):
-    content =subprocess.Popen(["hdfs", "dfs", "-cat", "/dataLake/Tisseo/open/*.json"], stdout=subprocess.PIPE)
+    content =subprocess.Popen(["hdfs", "dfs", "-cat", "/dataLake/Tisseo/soft_bounce/*.json"], stdout=subprocess.PIPE)
     context = {
         'content' : content.stdout
     }
