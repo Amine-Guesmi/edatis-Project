901705d4133a94a65b2b01ba4af4fcfa36be24c2
Amine-Guesmi <60828170+Amine-Guesmi@users.noreply.github.com>
2020-05-01 18:38:17 +0100

    lister les client

--- dashbord/models.py
+++ dashbord/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+class compte:
+    def __init__(self, id, name, bddname, active, inscriptiondate):
+        self.id = id
+        self.name = name
+        self.bddname = bddname
+        self.active = active
+        self.inscriptiondate = inscriptiondate
--- dashbord/views.py
+++ dashbord/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-import os, subprocess
+import os, subprocess, json
+import logging
+
+from .models import compte
+
+#logger instance
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 def dashbord(request):
-    content =subprocess.Popen(["hdfs", "dfs", "-cat", "/dataLake/Tisseo/soft_bounce/*.json"], stdout=subprocess.PIPE)
+    content =subprocess.Popen(["hdfs", "dfs", "-cat", "/dataLake/hub/compte/*.json"], stdout=subprocess.PIPE)
+    comptes = []
+
+    for line in content.stdout:
+        jsonTranform = json.loads(line)
+        comptes.append(compte(jsonTranform['id'],  jsonTranform['name'], jsonTranform['bddname'], jsonTranform['active'], jsonTranform['dateinscription']))
+
     context = {
-        'content' : content.stdout
+        'comptes' :  comptes
     }
     return render(request, 'dashbord.html', context)
